e836a6c0bf5b08ea6ba8e1d720c00b693b8b23e2
Courtney Nel <co.el.creates@gmail.com>
2024-03-01 12:52:11 -0500

    added routes and task controller

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("Welcome to server");
+const express = require('express');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/tasks', require('./routes/taskRoutes'));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
+
